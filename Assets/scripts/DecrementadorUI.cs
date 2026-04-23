746704ebd8888f7362d258a4121c8ed1f0d0ff86
Dantestriglio <Dantestriglio@gmail.com>
2026-04-23 16:47:57 -0300

    lajsdooiajhhhjsodi

--- Assets/scripts/DecrementadorUI.cs
+++ Assets/scripts/DecrementadorUI.cs
@@ -8,36 +8,19 @@ public class DecrementadorUI : MonoBehaviour
 {
     public TMP_InputField input_ingresoNumero;
     public TextMeshProUGUI txt_Resultado;
-
-
-        // Start is called before the first frame update
     void Start()
     {
-
-
     }
-
-  // Update is called once per frame
     void Update()
     {
-       
     }
-
-
-    public void Decrementar()
+     public void Decrementar()
     {
     int numero = int.Parse(input_ingresoNumero.text);
 
-
-
-
-    if (numero > 0)
+     if (numero > 0)
     {
         int resultado = numero - 1;
-
-
-
-
        input_ingresoNumero.text = resultado.ToString();
         txt_Resultado.text = "Resultado: " + resultado;
     }
@@ -45,7 +28,5 @@ public class DecrementadorUI : MonoBehaviour
     {
         txt_Resultado.text = "No puede ser menor que 0";
     }
-
-
-    }
+     }
 }
